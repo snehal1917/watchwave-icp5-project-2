52ef71f14f8f52c9fa793e715d1431b5d33c3dcd
noorshaikh123 <138210277+noorshaikh123@users.noreply.github.com>
2023-08-19 09:12:37 +0530

    added basic changes to button (#25)
    * added javascript file
    
    * created subpage for tollywood page
    
    * removed tollywood.html
    
    * created hover effects
    
    * added play button and add button

---
+++ pages/javascript.js
@@ -0,0 +1,38 @@
+function load()
+{
+    let loadelement = Math.round(Math.random()*9+1);
+    if(loadelement==1)
+    {alert("Try new movies of south cinema 🤩🤩");}
+
+    else if(loadelement==2)
+    {alert("Whats today's plan for movies 😁😁?");}
+
+    else if(loadelement==3)
+    {alert("Watch the new arrival of tollywood 😎😎?");}
+
+    else if(loadelement==4)
+    {alert("The action you require in your life is all here 🤺🤺🤺 ?");}
+
+    else if(loadelement==5)
+    {alert("The old school romance 😍 movie you would love to watch is here!!");}
+
+    else if(loadelement==6)
+    {alert("Pushpa says: ZHUKEGA NHI SAALA!! 😂😂");}
+
+    else if(loadelement==7)
+    {alert("VIOLENCE VIOLENCE VIOLENCE, I don't like it I avoid, But VIOLENCE Likes me I can't avoid!! 😎😎");}
+
+    else if(loadelement==8)
+    {alert("What about spending your quality time on thrilling movies??");}
+
+    else if(loadelement==9)
+    {alert("Arival of new south movies, Don't miss");}
+
+    else 
+    {alert("Give yourself peaceful time by watching our tollwood new movies?");}
+}
+
+function watchlist()
+{
+    alert('Movie has been Added to Watchlist');
+}
